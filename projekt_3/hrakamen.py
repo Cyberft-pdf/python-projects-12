fc1f887527de98bc1ebf8feda7c2f63886af24e3
Cyberft-pdf <80000156+Cyberft-pdf@users.noreply.github.com>
2022-12-21 22:06:35 +0100

    Dokončení
    Vymazání testů/zbytečností

--- projekt_3/hrakamen.py
+++ projekt_3/hrakamen.py
@@ -1,15 +1,11 @@
 import random 
-jmeno = input("Napiš jak se jmenuješ:")
-print(jmeno)
-
 
 def play():
     user = input("Jaká je tvoje volba: R pro kámen, p pro papír, s pro nuzky\n")
     computer = random.choice(["r", "p", "s"])
-    print("ahoj1")
     if user == computer:
         return "Je\" remíza"
-    print("ahoj2")
+
     if is_win(user, computer):
         return "Ty si vyhrál"
     print("ahoj3")
@@ -19,10 +15,10 @@ def play():
 def is_win(player, opponent):
     if (player == "r" and opponent == "s") or (player == "s" and opponent == "p") \
         or (player == "p" and opponent == "r"):
-        print("ahoj4")
+
         return True 
 
 print(play())
 
-#z nějákeho důvodu to nefunguje, fakt nevím :(
+
 
