c615b89ec2140882c87a87e0fa2366b7e053c142
Cyberft-pdf <80000156+Cyberft-pdf@users.noreply.github.com>
2022-12-23 20:38:18 +0100

    Update hangman.py

--- projekt_4/hangman.py
+++ projekt_4/hangman.py
@@ -19,7 +19,6 @@ def hangman():
     used_letters = set()
 
     lives = 6
-
     while len(word_letters) > 0 and lives > 0:
         print("Zbýváti",lives, "životů už si použil tyhle slova: ", " ". join(used_letters))
         word_list= [letter if letter in used_letters else "-" for letter in word] 
@@ -40,7 +39,12 @@ def hangman():
 
         else:
             print("Neplatný charakter. Pouzí jiný")
+    if lives == 0:
+        print("Promin, prohrál si:(", word)
+    else:
+        print("Uhodl si správné slovo", word, ":)")
 
+        
 hangman()
 
 
