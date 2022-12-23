a03d9878ea47cce3ef5db0102af082ef0c5eb2cd
Cyberft-pdf <80000156+Cyberft-pdf@users.noreply.github.com>
2022-12-23 18:24:18 +0100

    hangman
    změna
    konecně funguje

--- projekt_4/hangman.py
+++ projekt_4/hangman.py
@@ -18,18 +18,29 @@ def hangman():
     alphabet = set(string.ascii_uppercase)
     used_letters = set()
 
-    user_letter = input("Typlni si písmeno:").upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(used_letters)
+    lives = 6
 
-    elif user_letter in used_letters:
-        print("Tohle písmeno sí uz použil. Pouzí jiný")
+    while len(word_letters) > 0 and lives > 0:
+        print("Zbýváti",lives, "životů už si použil tyhle slova: ", " ". join(used_letters))
+        word_list= [letter if letter in used_letters else "-" for letter in word] 
+        print("Slovo které máte ted: "," ".join(word_list))
 
-    else:
-        print("Neplatný charakter. Pouzí jiný")
 
-print(hangman())
+        user_letter = input("Typlni si písmeno:").upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives = lives - 1 
+                print("Písmenko není ve slově.")
+
+        elif user_letter in used_letters:
+            print("Tohle písmeno sí uz použil. Pouzí jiný")
+
+        else:
+            print("Neplatný charakter. Pouzí jiný")
+
+hangman()
 
 
